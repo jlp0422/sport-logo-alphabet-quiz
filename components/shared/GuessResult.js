6af8a9e8ceee6ca32824cf9e8dc12742f0abcd6d
Jeremy Philipson <jeremyphilipson@gmail.com>
2020-12-15 19:22:18 -0500

    chore: more config

--- components/shared/GuessResult.js
+++ components/shared/GuessResult.js
@@ -1,30 +1,29 @@
 const InProgressGame = ({ isCorrectGuess, isIncorrectGuess, activeLogo }) => {
   const letterForLogo = activeLogo.letters[0]
   const teamName = activeLogo.title.split(' ').join('&nbsp;')
-  const hasGuessed = isCorrectGuess || isIncorrectGuess
 
   const [preGuessCopy, color] = isCorrectGuess
     ? ["That's correct!", 'green']
     : ["Nope, that's not correct!", 'red']
 
-  const copy = (
-    <span
-      dangerouslySetInnerHTML={{
-        __html: `${preGuessCopy} ${letterForLogo} is for ${teamName}.`
-      }}
-    />
-  )
+  const bgColorMap = {
+    green: 'bg-green-600',
+    red: 'bg-red-600'
+  }
 
-  const textColorMap = {
-    green: 'text-green-600',
-    red: 'text-red-600'
+  if (!(isCorrectGuess || isIncorrectGuess)) {
+    return null
   }
 
   return (
     <h2
-      className={`text-xl sm:text-2xl text-center py-2 font-bold ${textColorMap[color]}`}
+      className={`text-md xs:text-lg sm:text-2xl text-center font-bold text-white rounded-lg my-2 sm:mt-0 py-2 px-4 border-2 border-white border-solid ${bgColorMap[color]}`}
     >
-      {hasGuessed ? copy : <span>&nbsp;</span>}
+      <span
+        dangerouslySetInnerHTML={{
+          __html: `${preGuessCopy} ${letterForLogo} is for ${teamName}.`
+        }}
+      />
     </h2>
   )
 }
--- components/shared/LogoDisplay.js
+++ components/shared/LogoDisplay.js
@@ -8,19 +8,13 @@ const LogoDisplay = ({ activeLogo, ...props }) => {
   }, [activeLogo])
 
   return (
-    <div className='m-2 p-4 bg-white rounded-md border shadow w-4/6 xsm:w-max'>
-      {isLoading && (
-        <h2
-          className='flex justify-center items-center font-bold text-2xl text-center'
-          style={{
-            width: 225,
-            height: 230
-          }}
-        >
+    <div className='m-2 p-4 bg-white rounded-md border shadow w-1/2 xs:w-max'>
+      {true && (
+        <h2 className='flex justify-center items-center xs:font-bold text-lg xs:text-2xl text-center w-full h-full xs:w-225 xs:h-230'>
           Grabbing the next logo...
         </h2>
       )}
-      <div className={isLoading ? 'hidden' : 'block'}>
+      <div className={true ? 'hidden' : 'block'}>
         <Logo {...props} logo={activeLogo} setIsLoading={setIsLoading} />
       </div>
     </div>
