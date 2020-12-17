fd13fb83782401894d729ebec924e9d2b681797e
Jeremy Philipson <jeremyphilipson@gmail.com>
2020-12-16 20:54:42 -0500

    chore: more things around

--- constants/actions.js
+++ constants/actions.js
@@ -1,7 +1,9 @@
-export const SET_ACTIVE_LOGO = { type: 'SET_ACTIVE_LOGO' }
-export const SET_LOGOS = { type: 'SET_LOGOS' }
-export const START_GAME = { type: 'START_GAME' }
-export const CORRECT_GUESS = { type: 'CORRECT_GUESS' }
-export const INCORRECT_GUESS = { type: 'INCORRECT_GUESS' }
-export const RESET_GAME = { type: 'RESET_GAME' }
-export const GAME_OVER = { type: 'GAME_OVER' }
+import * as types from './types'
+
+export const SET_ACTIVE_LOGO = { type: types.SET_ACTIVE_LOGO }
+export const SET_LOGOS = { type: types.SET_LOGOS }
+export const START_GAME = { type: types.START_GAME }
+export const CORRECT_GUESS = { type: types.CORRECT_GUESS }
+export const INCORRECT_GUESS = { type: types.INCORRECT_GUESS }
+export const RESET_GAME = { type: types.RESET_GAME }
+export const GAME_OVER = { type: types.GAME_OVER }
---
+++ constants/types.js
@@ -0,0 +1,7 @@
+export const SET_ACTIVE_LOGO = 'SET_ACTIVE_LOGO'
+export const SET_LOGOS = 'SET_LOGOS'
+export const START_GAME = 'START_GAME'
+export const CORRECT_GUESS = 'CORRECT_GUESS'
+export const INCORRECT_GUESS = 'INCORRECT_GUESS'
+export const RESET_GAME = 'RESET_GAME'
+export const GAME_OVER = 'GAME_OVER'
