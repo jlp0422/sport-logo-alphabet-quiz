9331f67d54b91c67c2a7d1cebddc403314ccc238
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-01-15 20:51:20 -0500

    update basketball logos

--- constants/logos/basketball/logos.js
+++ constants/logos/basketball/logos.js
@@ -3,67 +3,61 @@ import { createNbaTeam, createWnbaTeam } from '../helpers'
 const brooklynNets = createNbaTeam({
   title: 'Brooklyn Nets',
   letters: ['B'],
-  answers: ['nets', 'brooklyn', 'brooklyn nets']
+  answers: ['nets', 'brooklyn']
 })
 
 const clevelandCavaliers = createNbaTeam({
   title: 'Cleveland Cavaliers',
   letters: ['C'],
-  answers: ['cavs', 'cleveland', 'cavaliers', 'cleveland cavaliers']
+  answers: ['cavs', 'cleveland', 'cavaliers']
 })
 
 const utahJazz = createNbaTeam({
   title: 'Utah Jazz',
   letters: ['J'],
-  answers: ['jazz', 'utah', 'utah jazz']
+  answers: ['jazz', 'utah']
 })
 
 const losAngelesLakers = createNbaTeam({
   title: 'Los Angeles Lakers',
   letters: ['L'],
-  answers: ['lakers', 'los angeles', 'la lakers', 'los angeles lakers']
+  answers: ['lakers', 'los angeles', 'la']
 })
 
 const charlotteBobcats = createNbaTeam({
   title: 'Charlotte Bobcats',
   letters: ['C'],
-  answers: ['charlotte', 'bobcats', 'charlotte bobcats']
+  answers: ['charlotte', 'bobcats']
 })
 
 const vancouverGrizzlies = createNbaTeam({
   title: 'Vancouver Grizzlies',
   letters: ['G'],
-  answers: ['vancouver', 'grizzlies', 'vancouver grizzlies']
+  answers: ['vancouver', 'grizzlies']
 })
 
 const detroitPistons = createNbaTeam({
   title: 'Detroit Pistons',
   letters: ['P'],
-  answers: ['detroit', 'pistons', 'detroit pistons']
+  answers: ['detroit', 'pistons']
 })
 
 const indianaPacers = createNbaTeam({
   title: 'Indiana Pacers',
   letters: ['P'],
-  answers: ['indiana', 'pacers', 'indiana pacers']
+  answers: ['indiana', 'pacers']
 })
 
 const houstonRockets = createNbaTeam({
   title: 'Houston Rockets',
   letters: ['R'],
-  answers: ['houston', 'rockets', 'houston rockets']
+  answers: ['houston', 'rockets']
 })
 
 const seattleSuperSonics = createNbaTeam({
   title: 'Seattle SuperSonics',
   letters: ['S'],
-  answers: [
-    'seattle',
-    'supersonics',
-    'super sonics',
-    'seattle supersonics',
-    'seattle super sonics'
-  ]
+  answers: ['seattle', 'supersonics', 'super sonics']
 })
 
 export const NBA_LOGOS = [
@@ -82,37 +76,37 @@ export const NBA_LOGOS = [
 const atlantaDream = createWnbaTeam({
   title: 'Atlanta Dream',
   letters: ['A'],
-  answers: ['atlanta', 'dream', 'atlanta dream']
+  answers: ['atlanta', 'dream']
 })
 
 const indianaFever = createWnbaTeam({
   title: 'Indiana Fever',
   letters: ['F'],
-  answers: ['indiana', 'fever', 'indiana fever']
+  answers: ['indiana', 'fever']
 })
 
 const minnesotaLynx = createWnbaTeam({
   title: 'Minnesota Lynx',
   letters: ['M'],
-  answers: ['minnesota', 'lynx', 'minnesota lynx']
+  answers: ['minnesota', 'lynx']
 })
 
 const phoenixMercury = createWnbaTeam({
   title: 'Phoenix Mercury',
   letters: ['M'],
-  answers: ['mercury', 'phoenix', 'phoenix mercury']
+  answers: ['mercury', 'phoenix']
 })
 
 const chicagoSky = createWnbaTeam({
   title: 'Chicago Sky',
   letters: ['S'],
-  answers: ['chicago', 'sky', 'chicago sky']
+  answers: ['chicago', 'sky']
 })
 
 const seattleStorm = createWnbaTeam({
   title: 'Seattle Storm',
   letters: ['S'],
-  answers: ['seattle', 'storm', 'seattle storm']
+  answers: ['seattle', 'storm']
 })
 
 export const WNBA_LOGOS = [
