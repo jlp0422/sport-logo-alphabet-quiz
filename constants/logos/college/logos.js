053bf0485e7048760fdfab70acdbbfc2e5802005
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-01-20 18:29:33 -0500

    finish college logos yay

--- constants/logos/college/logos.js
+++ constants/logos/college/logos.js
@@ -3,7 +3,7 @@ import { createCollegeTeam } from '../helpers'
 const alabama = createCollegeTeam({
   title: 'Alabama Crimson Tide',
   letters: ['A'],
-  answers: ['alabama', 'bama', 'crimson tide']
+  answers: ['alabama', 'bama', 'crimson tide', 'roll tide']
 })
 
 const appState = createCollegeTeam({
@@ -117,7 +117,7 @@ const easternMichigan = createCollegeTeam({
 const etsu = createCollegeTeam({
   title: 'ETSU Buccaneers',
   letters: ['E'],
-  answers: ['eastern tennessee state university', 'buccaneers', 'bucs', 'etsu']
+  answers: ['east tennessee state university', 'buccaneers', 'bucs', 'etsu']
 })
 
 const florida = createCollegeTeam({
@@ -135,7 +135,7 @@ const fordham = createCollegeTeam({
 const georgia = createCollegeTeam({
   title: 'Georgia Bulldogs',
   letters: ['G'],
-  answers: ['georgia', 'bulldogs']
+  answers: ['georgia', 'bulldogs', 'go dawgs']
 })
 
 const hartford = createCollegeTeam({
@@ -159,7 +159,7 @@ const hawaii = createCollegeTeam({
 const idaho = createCollegeTeam({
   title: 'Idaho Vandals',
   letters: ['I'],
-  answers: ['idaho']
+  answers: ['idaho', 'vandals']
 })
 
 const illinois = createCollegeTeam({
@@ -171,7 +171,7 @@ const illinois = createCollegeTeam({
 const indianaState = createCollegeTeam({
   title: 'Indiana State Sycamores',
   letters: ['I'],
-  answers: ['indiana state']
+  answers: ['indiana state', 'sycamores']
 })
 
 const lafayette = createCollegeTeam({
@@ -195,7 +195,7 @@ const loyalaMaryland = createCollegeTeam({
 const manhattan = createCollegeTeam({
   title: 'Manhattan Jaspers',
   letters: ['M'],
-  answers: ['manhattan']
+  answers: ['manhattan', 'jaspers']
 })
 
 const mercer = createCollegeTeam({
@@ -231,7 +231,7 @@ const minnesota = createCollegeTeam({
 const mississippiState = createCollegeTeam({
   title: 'Mississippi State Bulldogs',
   letters: ['M'],
-  answers: ['state', 'mississippi state', 'bulldogs']
+  answers: ['mississippi state', 'bulldogs']
 })
 
 const murrayState = createCollegeTeam({
@@ -274,6 +274,168 @@ const oregon = createCollegeTeam({
   title: 'Oregon Ducks',
   letters: ['O'],
   answers: ['oregon', 'ducks']
+})
+
+const penn = createCollegeTeam({
+  title: 'Penn Quakers',
+  letters: ['P'],
+  answers: ['penn', 'upenn', 'quakers', 'pennsylvania']
+})
+
+const portland = createCollegeTeam({
+  title: 'Portland Pilots',
+  letters: ['P'],
+  answers: ['portland', 'pilots']
+})
+
+const princeton = createCollegeTeam({
+  title: 'Princeton Tigers',
+  letters: ['P'],
+  answers: ['princeton', 'tigers']
+})
+
+const purdue = createCollegeTeam({
+  title: 'Purdue Boilermakers',
+  letters: ['P'],
+  answers: ['purdue', 'boilermakers']
+})
+
+const rice = createCollegeTeam({
+  title: 'Rice Owls',
+  letters: ['R'],
+  answers: ['rice', 'owls']
+})
+
+const rutgers = createCollegeTeam({
+  title: 'Rutgers Scarlet Knights',
+  letters: ['R'],
+  answers: ['rutgers', 'scarlet knights']
+})
+
+const sacState = createCollegeTeam({
+  title: 'Sacramento State Hornets',
+  letters: ['S'],
+  answers: ['sacramento state', 'sacramento', 'hornets']
+})
+
+const siena = createCollegeTeam({
+  title: 'Siena Saints',
+  letters: ['S'],
+  answers: ['siena', 'saints']
+})
+
+const usf = createCollegeTeam({
+  title: 'South Florida Bulls',
+  letters: ['U'],
+  answers: ['usf', 'bulls', 'south florida', 'south florida']
+})
+
+const stanford = createCollegeTeam({
+  title: 'Stanford Cardinal',
+  letters: ['S'],
+  answers: ['stanford', 'cardinal']
+})
+
+const stetson = createCollegeTeam({
+  title: 'Stetson Hatters',
+  letters: ['S'],
+  answers: ['stetson', 'hatters']
+})
+
+const syracuse = createCollegeTeam({
+  title: 'Syracuse Orange',
+  letters: ['S'],
+  answers: ['syracuse', 'orange', 'cuse']
+})
+
+const temple = createCollegeTeam({
+  title: 'Temple Owls',
+  letters: ['T'],
+  answers: ['temple', 'owls']
+})
+
+const tennessee = createCollegeTeam({
+  title: 'Tennessee Volunteers',
+  letters: ['T'],
+  answers: ['tennessee', 'volunteers']
+})
+
+const texasTech = createCollegeTeam({
+  title: 'Texas Tech Red Raiders',
+  letters: ['T'],
+  answers: ['texas tech', 'raiders', 'red raiders']
+})
+
+const citadel = createCollegeTeam({
+  title: 'The Citadel Bulldogs',
+  letters: ['C'],
+  answers: ['the citadel', 'bulldogs', 'citadel']
+})
+
+const troy = createCollegeTeam({
+  title: 'Troy Trojans',
+  letters: ['T'],
+  answers: ['troy', 'trojans']
+})
+
+const union = createCollegeTeam({
+  title: 'Union Dutchmen',
+  letters: ['U'],
+  answers: ['union', 'dutchmen']
+})
+
+const utah = createCollegeTeam({
+  title: 'Utah Utes',
+  letters: ['U'],
+  answers: ['utah', 'utes']
+})
+
+const vandy = createCollegeTeam({
+  title: 'Vanderbilt Commodores',
+  letters: ['V'],
+  answers: ['vandy', 'vanderbilt', 'dores', 'commodores']
+})
+
+const vermont = createCollegeTeam({
+  title: 'Vermont Catamounts',
+  letters: ['V'],
+  answers: ['vermont', 'catamounts']
+})
+
+const nova = createCollegeTeam({
+  title: 'Villanova Wildcats',
+  letters: ['V'],
+  answers: ['nova', 'villanova', 'wildcats']
+})
+
+const virginia = createCollegeTeam({
+  title: 'Virginia Cavaliers',
+  letters: ['V'],
+  answers: ['virginia', 'cavaliers']
+})
+
+const wisconsin = createCollegeTeam({
+  title: 'Wisconsin Badgers',
+  letters: ['W'],
+  answers: ['wisconsin', 'badgers']
+})
+
+const xavier = createCollegeTeam({
+  title: 'Xavier Musketeers',
+  letters: ['X'],
+  answers: ['xavier', 'musketeers']
+})
+
+const yale = createCollegeTeam({
+  title: 'Yale Bulldogs',
+  letters: ['Y'],
+  answers: ['yale', 'bulldogs']
+})
+
+const youngstownState = createCollegeTeam({
+  title: 'Youngstown State Penguins',
+  letters: ['y'],
+  answers: ['youngstown state', 'youngstown', 'penguins']
 })
 
 export const COLLEGE_LOGOS = [
@@ -321,5 +483,32 @@ export const COLLEGE_LOGOS = [
   ncAsheville,
   northwestern,
   ohioState,
-  oregon
+  oregon,
+  penn,
+  portland,
+  princeton,
+  purdue,
+  rice,
+  rutgers,
+  sacState,
+  siena,
+  usf,
+  stanford,
+  stetson,
+  syracuse,
+  temple,
+  tennessee,
+  texasTech,
+  citadel,
+  troy,
+  union,
+  utah,
+  vandy,
+  vermont,
+  nova,
+  virginia,
+  wisconsin,
+  xavier,
+  yale,
+  youngstownState
 ]
